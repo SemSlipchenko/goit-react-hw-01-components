08dc2fc8fc1001320ab58e8cec4211a08eee16cb
SemSlipchenko <semslipchenko@gmail.com>
2022-10-23 17:39:58 +0300

    adding profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      avatar={user.avatar}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      followers={user.stats.followers.toLocaleString('ru')}
+      views={user.stats.views.toLocaleString('ru')}
+      likes={user.stats.likes.toLocaleString('ru')}
+    />
   );
 };
