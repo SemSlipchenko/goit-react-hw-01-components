af0c2c739df2d05ef6440d5a7ded412f1edff5fa
SemSlipchenko <semslipchenko@gmail.com>
2022-10-24 20:29:48 +0300

    adding component transactions

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import css from 'App.module.css';
 import { Profile } from './components/Profile/Profile';
 import { Statistics } from './components/Statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} className={css.tableRow}>
+              <td className={css.tableData}>{type}</td>
+              <td className={css.tableData}>{amount}</td>
+              <td className={css.tableData}>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
